df76c6cca3291d6a383c9007da67129f9aa11511
E4XeniTh <toniichaan@gmail.com>
2024-01-08 11:28:19 +0100

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,142 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        TextBox1 = New TextBox()
+        btnUpdate = New Button()
+        BtnToggleStatus = New Button()
+        ListView1 = New ListView()
+        ContextMenuStrip1 = New ContextMenuStrip(components)
+        LocationPathTextBox = New TextBox()
+        SetLocationBtn = New Button()
+        GroupBox1 = New GroupBox()
+        GroupBox1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Enabled = False
+        TextBox1.Location = New Point(12, 546)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(256, 23)
+        TextBox1.TabIndex = 1
+        ' 
+        ' btnUpdate
+        ' 
+        btnUpdate.Enabled = False
+        btnUpdate.Location = New Point(272, 545)
+        btnUpdate.Name = "btnUpdate"
+        btnUpdate.Size = New Size(100, 25)
+        btnUpdate.TabIndex = 2
+        btnUpdate.Text = "Rename"
+        btnUpdate.UseVisualStyleBackColor = True
+        ' 
+        ' BtnToggleStatus
+        ' 
+        BtnToggleStatus.Enabled = False
+        BtnToggleStatus.Location = New Point(12, 494)
+        BtnToggleStatus.Name = "BtnToggleStatus"
+        BtnToggleStatus.Size = New Size(360, 25)
+        BtnToggleStatus.TabIndex = 2
+        BtnToggleStatus.Text = "Toggle"
+        BtnToggleStatus.UseVisualStyleBackColor = True
+        ' 
+        ' ListView1
+        ' 
+        ListView1.FullRowSelect = True
+        ListView1.Location = New Point(12, 78)
+        ListView1.Name = "ListView1"
+        ListView1.Size = New Size(360, 410)
+        ListView1.TabIndex = 5
+        ListView1.UseCompatibleStateImageBehavior = False
+        ListView1.View = View.Details
+        ' 
+        ' ContextMenuStrip1
+        ' 
+        ContextMenuStrip1.Name = "ContextMenuStrip1"
+        ContextMenuStrip1.Size = New Size(61, 4)
+        ' 
+        ' LocationPathTextBox
+        ' 
+        LocationPathTextBox.Location = New Point(18, 29)
+        LocationPathTextBox.Name = "LocationPathTextBox"
+        LocationPathTextBox.Size = New Size(294, 23)
+        LocationPathTextBox.TabIndex = 1
+        ' 
+        ' SetLocationBtn
+        ' 
+        SetLocationBtn.Location = New Point(304, 19)
+        SetLocationBtn.Name = "SetLocationBtn"
+        SetLocationBtn.Size = New Size(51, 23)
+        SetLocationBtn.TabIndex = 9
+        SetLocationBtn.Text = "Set"
+        SetLocationBtn.UseVisualStyleBackColor = True
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Controls.Add(SetLocationBtn)
+        GroupBox1.Location = New Point(12, 9)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(361, 54)
+        GroupBox1.TabIndex = 10
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "Mod Path"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7.0F, 15.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(384, 581)
+        Controls.Add(ListView1)
+        Controls.Add(btnUpdate)
+        Controls.Add(LocationPathTextBox)
+        Controls.Add(TextBox1)
+        Controls.Add(BtnToggleStatus)
+        Controls.Add(GroupBox1)
+        FormBorderStyle = FormBorderStyle.FixedToolWindow
+        MaximumSize = New Size(400, 700)
+        MinimumSize = New Size(400, 600)
+        Name = "Form1"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Sorting of Isaac"
+        GroupBox1.ResumeLayout(False)
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents btnUpdate As Button
+    Friend WithEvents BtnToggleStatus As Button
+    Friend WithEvents btnMoveUp As Button
+    Friend WithEvents btnMoveDown As Button
+    Friend WithEvents btnMoveTop As Button
+    Friend WithEvents btnMoveBottom As Button
+    Friend WithEvents ListView1 As ListView
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents LocationPathTextBox As TextBox
+    Friend WithEvents SetLocationBtn As Button
+    Friend WithEvents GroupBox1 As GroupBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,178 @@
+﻿Imports System.IO
+Imports System.Xml
+
+Public Class Form1
+
+    Dim metadataFiles As String()
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        AddHandler TextBox1.KeyDown, AddressOf TextBox1_KeyDown
+        LocationPathTextBox.Text = ""
+    End Sub
+
+    Private Sub SetLocationBtn_Click(sender As Object, e As EventArgs) Handles SetLocationBtn.Click
+        InitializeListView()
+    End Sub
+
+    Private Sub InitializeListView()
+        ' Set up the ListView
+        If Directory.Exists(LocationPathTextBox.Text) Then
+            ListView1.Columns.Add("Name (Load order)", 240)
+            ListView1.Columns.Add("Status", 80)
+            metadataFiles = Directory.GetFiles(LocationPathTextBox.Text, "metadata.xml", SearchOption.AllDirectories)
+            LocationPathTextBox.Enabled = False
+            SetLocationBtn.Enabled = False
+            LoadData()
+            SortListViewItems(0)
+        Else
+            MessageBox.Show("Location doesn't exist.", "Error",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error)
+            With LocationPathTextBox
+                .Focus()
+                .SelectAll()
+            End With
+            Exit Sub
+        End If
+    End Sub
+
+    Private Sub LoadData()
+        ListView1.Items.Clear()
+
+        For Each metadataFile As String In metadataFiles
+            Dim doc As New XmlDocument()
+            doc.Load(metadataFile)
+
+            Dim nameNode As XmlNode = doc.SelectSingleNode("/metadata/name")
+            If nameNode IsNot Nothing Then
+                Dim subfolderPath = Path.GetDirectoryName(metadataFile)
+                Dim isEnabled As Boolean = Not File.Exists(Path.Combine(subfolderPath, "disable.it"))
+
+                Dim item As New ListViewItem(nameNode.InnerText)
+                item.SubItems.Add(If(isEnabled, "Enabled", "Disabled"))
+
+                ListView1.Items.Add(item)
+            End If
+        Next
+    End Sub
+
+    Private Sub ListView1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListView1.SelectedIndexChanged
+        If ListView1.SelectedItems.Count = 1 Then
+            Dim selectedItem As ListViewItem = ListView1.SelectedItems(0)
+            Dim selectedName As String = selectedItem.SubItems(0).Text
+            TextBox1.Text = selectedName
+            btnUpdate.Enabled = True
+            TextBox1.Enabled = True
+            BtnToggleStatus.Enabled = True
+        ElseIf ListView1.SelectedItems.Count > 1 Then
+            TextBox1.Text = ""
+            TextBox1.Enabled = False
+            btnUpdate.Enabled = False
+            BtnToggleStatus.Enabled = True
+        Else
+            TextBox1.Text = ""
+            TextBox1.Enabled = False
+            btnUpdate.Enabled = False
+            BtnToggleStatus.Enabled = False
+        End If
+    End Sub
+    Private Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
+        If ListView1.SelectedItems.Count > 0 Then
+            Dim selectedItem As ListViewItem = ListView1.SelectedItems(0)
+            Dim selectedName As String = selectedItem.SubItems(0).Text
+            Dim newName As String = TextBox1.Text
+
+            For Each metadataFile As String In Directory.GetFiles(LocationPathTextBox.Text, "metadata.xml", SearchOption.AllDirectories)
+                Dim doc As New XmlDocument()
+                doc.Load(metadataFile)
+
+                Dim nameNode As XmlNode = doc.SelectSingleNode("/metadata/name")
+                If nameNode IsNot Nothing AndAlso nameNode.InnerText = selectedName Then
+                    ' Update the <name> tag
+                    nameNode.InnerText = newName
+                    doc.Save(metadataFile)
+                End If
+            Next
+
+            LoadData()
+            TextBox1.Text = ""
+            TextBox1.Enabled = False
+            btnUpdate.Enabled = False
+            BtnToggleStatus.Enabled = False
+        End If
+    End Sub
+
+    Private Sub btnToggleStatus_Click(sender As Object, e As EventArgs) Handles BtnToggleStatus.Click
+        For Each selectedItem As ListViewItem In ListView1.SelectedItems
+            Dim selectedName As String = selectedItem.SubItems(0).Text
+
+            For Each metadataFile As String In Directory.GetFiles(LocationPathTextBox.Text, "metadata.xml", SearchOption.AllDirectories)
+                Dim doc As New XmlDocument()
+                doc.Load(metadataFile)
+
+                Dim nameNode As XmlNode = doc.SelectSingleNode("/metadata/name")
+                If nameNode IsNot Nothing AndAlso nameNode.InnerText = selectedName Then
+                    ' Enable/disable the subfolder based on the current status
+                    Dim subfolderPath = Path.GetDirectoryName(metadataFile)
+                    Dim disableFilePath = Path.Combine(subfolderPath, "disable.it")
+
+                    If selectedItem.SubItems(1).Text = "Disabled" Then
+                        If File.Exists(disableFilePath) Then
+                            File.Delete(disableFilePath)
+                        End If
+                    Else
+                        File.Create(disableFilePath).Dispose()
+                    End If
+                End If
+            Next
+        Next
+
+        LoadData()
+        TextBox1.Text = ""
+        TextBox1.Enabled = False
+        btnUpdate.Enabled = False
+        BtnToggleStatus.Enabled = False
+    End Sub
+    Private Sub ListView1_ColumnClick(sender As Object, e As ColumnClickEventArgs) Handles ListView1.ColumnClick
+        If e.Column = 0 Then
+            SortListViewItems(0) ' Sort by Name column
+        ElseIf e.Column = 1 Then
+            SortListViewItems(1) ' Sort by Status column
+        End If
+    End Sub
+
+    Private Sub SortListViewItems(columnIndex As Integer)
+        Dim comparer As New ListViewItemComparer(columnIndex)
+        ListView1.ListViewItemSorter = comparer
+        ListView1.Sort()
+    End Sub
+    Public Class ListViewItemComparer
+        Implements IComparer
+
+        Private columnIndex As Integer
+
+        Public Sub New(columnIndex As Integer)
+            Me.columnIndex = columnIndex
+        End Sub
+
+        Public Function Compare(x As Object, y As Object) As Integer Implements IComparer.Compare
+            Dim itemX As ListViewItem = DirectCast(x, ListViewItem)
+            Dim itemY As ListViewItem = DirectCast(y, ListViewItem)
+
+            Dim valueX As String = itemX.SubItems(columnIndex).Text
+            Dim valueY As String = itemY.SubItems(columnIndex).Text
+
+            If columnIndex = 1 Then ' Status column
+                Return String.Compare(valueY, valueX) ' Reverse the order
+            Else
+                Return String.Compare(valueX, valueY)
+            End If
+        End Function
+    End Class
+    Private Sub TextBox1_KeyDown(sender As Object, e As KeyEventArgs) Handles TextBox1.KeyDown, LocationPathTextBox.KeyDown
+        If e.KeyCode = Keys.Enter And ListView1.SelectedItems.Count = 1 Then
+            ' Trigger the click event of the Rename button
+            btnUpdate.PerformClick()
+            e.SuppressKeyPress = True ' Suppress the Enter key press event
+        End If
+    End Sub
+End Class
